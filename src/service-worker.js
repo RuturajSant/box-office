62f4332d29e7799ce79603e15f5156385a7e6910
Ruturaj <sant.ruturaj2001@gmail.com>
2021-07-16 10:37:13 +0530

    initial commit

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-restricted-globals */
+/* eslint-disable */
+/* eslint-disable no-restricted-globals */ 
 
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
